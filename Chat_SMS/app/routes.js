7e89c0da4dd9a11a46ecb06d634892764cb87dcf
NLDP <1312422@student.hcmus.edu.vn>
2016-05-03 10:03:49 +0700

    Sigin_Sigout

---
+++ Chat_SMS/app/routes.js
@@ -0,0 +1,35 @@
+module.exports=function(app,passport){
+
+//Home page
+
+	app.get('/',function(req,res){
+		res.render('Page/index.ejs');
+	});
+
+//Login
+
+	app.get('/login',function(ref,res){
+		res.render('Partials/login.ejs',{message:req.flash('loginMessage')})
+	});
+
+
+	app.get('/signup', function(req,res){
+		res.render('Partials/signup.ejs',{message: req.flash('signupMessage')})
+	});
+
+	app.get('/profile',isLoggedIn, function(req, res){
+		res.render('Partials/profile.ejs',{ user : req.user })
+	});
+
+	function isLoggedIn(req, res, next){
+		if(req.isAuthenticated())
+			return next();
+		res.redirect('/');
+	}
+
+	app.post('/signup',passport.authenticate('local-signup',{
+		successRedirect : '/Partials/profile',
+		failureRedirect : '/Partials/signup',
+		failureFlash : true
+	}));
+}
